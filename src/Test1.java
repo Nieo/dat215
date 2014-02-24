8a353a4e641d39d35bf475fe2cd78e4c6abb9b4f
Nieo <pihl_93@hotmail.com>
2014-02-23 20:13:25 -0800

    First try

---
+++ src/Test1.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Erik
+ */
+public class Test1 {
+    public static void main(String[] args){
+        System.out.print("Hello world");
+    }
+}
